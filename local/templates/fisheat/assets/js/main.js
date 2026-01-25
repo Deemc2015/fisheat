7d31604fed419a2d9841f910742e2651129aa928
Дмитрий Литвинов <Dlitvinovo@gmail.com>
2026-01-25 13:11:18 +0500

    footer mobile

--- local/templates/fisheat/assets/js/main.js
+++ local/templates/fisheat/assets/js/main.js
@@ -158,7 +158,32 @@ $(document).ready(function(){
         slidesToShow: 5,
         slidesToScroll: 1,
         dots:false,
-        arrows:false
+        arrows:false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ],
     })
 
 
