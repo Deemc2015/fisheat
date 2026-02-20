d0c4c8c346106f20ba03974d488461655b09dcef
Дмитрий Литвинов <Dlitvinovo@gmail.com>
2026-02-20 16:16:22 +0500

    test

--- local/templates/fisheat/header.php
+++ local/templates/fisheat/header.php
@@ -9,7 +9,6 @@ use \Ldo\Develop\Pages;
 <!DOCTYPE html>
 <html>
 <head>
-434223444444234234
 <?$APPLICATION->ShowHead();?>
 <meta name="robots" content="noindex, nofollow" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
