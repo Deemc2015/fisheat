c9751647e4dd1fa1acd2c4a112e45de91ddba7b6
ftp <arlusyzm_ssh@dark.beget.ru>
2026-02-21 14:03:38 +0300

    Перенос hl адресов

---
+++ local/php_interface/migrations/Version120260221140304.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version120260221140304 extends Version
+{
+    protected $author = "admin";
+
+    protected $description = "";
+
+    protected $moduleVersion = "5.6.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+    $hlblockId = $helper->Hlblock()->saveHlblock(array (
+  'NAME' => 'AdressUser',
+  'TABLE_NAME' => 'adress_user',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Адреса пользователей',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'User Adress',
+    ),
+  ),
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_SHIRINA',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => 'Ширина',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_DOLGOTA',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => 'Долгота',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_ADDRESS',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => 'Адрес',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_USER_ID',
+  'USER_TYPE_ID' => 'double',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'PRECISION' => 4,
+    'SIZE' => 20,
+    'MIN_VALUE' => 0.0,
+    'MAX_VALUE' => 0.0,
+    'DEFAULT_VALUE' => NULL,
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => 'ID пользователя',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+        }
+}
