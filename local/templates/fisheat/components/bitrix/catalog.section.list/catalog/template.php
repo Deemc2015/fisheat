46bbaa2beafa4e80f166f8d0bc139474182b3e5f
Дмитрий Литвинов <Dlitvinovo@gmail.com>
2026-03-24 19:15:00 +0500

    dev defailt catalog list

--- local/templates/fisheat/components/bitrix/catalog.section.list/catalog/template.php
+++ local/templates/fisheat/components/bitrix/catalog.section.list/catalog/template.php
@@ -12,225 +12,129 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-$arViewModeList = $arResult['VIEW_MODE_LIST'];
-
-$arViewStyles = array(
-	'LIST' => array(
-		'CONT' => 'bx_sitemap',
-		'TITLE' => 'bx_sitemap_title',
-		'LIST' => 'bx_sitemap_ul',
-	),
-	'LINE' => array(
-		'CONT' => 'bx_catalog_line',
-		'TITLE' => 'bx_catalog_line_category_title',
-		'LIST' => 'bx_catalog_line_ul',
-		'EMPTY_IMG' => $this->GetFolder().'/images/line-empty.png'
-	),
-	'TEXT' => array(
-		'CONT' => 'bx_catalog_text',
-		'TITLE' => 'bx_catalog_text_category_title',
-		'LIST' => 'bx_catalog_text_ul'
-	),
-	'TILE' => array(
-		'CONT' => 'bx_catalog_tile',
-		'TITLE' => 'bx_catalog_tile_category_title',
-		'LIST' => 'bx_catalog_tile_ul',
-		'EMPTY_IMG' => $this->GetFolder().'/images/tile-empty.png'
-	)
-);
-$arCurView = $arViewStyles[$arParams['VIEW_MODE']];
-
-$strSectionEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_EDIT");
-$strSectionDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_DELETE");
-$arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_CONFIRM'));
-
 ?>
 
-<div class="<? echo $arCurView['CONT']; ?>"><?
-if ('Y' == $arParams['SHOW_PARENT_NAME'] && 0 < $arResult['SECTION']['ID'])
-{
-	$this->AddEditAction($arResult['SECTION']['ID'], $arResult['SECTION']['EDIT_LINK'], $strSectionEdit);
-	$this->AddDeleteAction($arResult['SECTION']['ID'], $arResult['SECTION']['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
+<?foreach ($arResult['SECTIONS'] as &$arSection):?>
+	<h2 class="bx_catalog_tile_title"><?=$arSection['NAME']; ?></h2>
+    <?
 
-	?><h1
-		class="<? echo $arCurView['TITLE']; ?>"
-		id="<? echo $this->GetEditAreaId($arResult['SECTION']['ID']); ?>"
-	><a href="<? echo $arResult['SECTION']['SECTION_PAGE_URL']; ?>"><?
-		echo (
-			isset($arResult['SECTION']["IPROPERTY_VALUES"]["SECTION_PAGE_TITLE"]) && $arResult['SECTION']["IPROPERTY_VALUES"]["SECTION_PAGE_TITLE"] != ""
-			? $arResult['SECTION']["IPROPERTY_VALUES"]["SECTION_PAGE_TITLE"]
-			: $arResult['SECTION']['NAME']
-		);
-	?></a></h1><?
-}
-if (0 < $arResult["SECTIONS_COUNT"])
-{
-?>
-<ul class="<? echo $arCurView['LIST']; ?>">
-<?
-	switch ($arParams['VIEW_MODE'])
-	{
-		case 'LINE':
-			foreach ($arResult['SECTIONS'] as &$arSection)
-			{
-				$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-				$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-				if (false === $arSection['PICTURE'])
-				{
-					$altValue = (string)($arSection['IPROPERTY_VALUES']['SECTION_PICTURE_FILE_ALT'] ?? '');
-					if ($altValue === '')
-					{
-						$altValue = $arSection['NAME'];
-					}
-					$titleValue = (string)($arSection['IPROPERTY_VALUES']['SECTION_PICTURE_FILE_TITLE'] ?? '');
-					if ($titleValue === '')
-					{
-						$titleValue = $arSection['NAME'];
-					}
-					$arSection['PICTURE'] = array(
-						'SRC' => $arCurView['EMPTY_IMG'],
-						'ALT' => $altValue,
-						'TITLE' => $titleValue,
-					);
-					unset($titleValue, $altValue);
-				}
-				?><li id="<? echo $this->GetEditAreaId($arSection['ID']); ?>">
-				<a
-					href="<? echo $arSection['SECTION_PAGE_URL']; ?>"
-					class="bx_catalog_line_img"
-					style="background-image: url('<? echo $arSection['PICTURE']['SRC']; ?>');"
-					title="<? echo $arSection['PICTURE']['TITLE']; ?>"
-				></a>
-				<h2 class="bx_catalog_line_title"><a href="<? echo $arSection['SECTION_PAGE_URL']; ?>"><? echo $arSection['NAME']; ?></a><?
-				if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null)
-				{
-					?> <span>(<? echo $arSection['ELEMENT_CNT']; ?>)</span><?
-				}
-				?></h2><?
-				if ('' != $arSection['DESCRIPTION'])
-				{
-					?><p class="bx_catalog_line_description"><? echo $arSection['DESCRIPTION']; ?></p><?
-				}
-				?><div style="clear: both;"></div>
-				</li><?
-			}
-			unset($arSection);
-			break;
-		case 'TEXT':
-			foreach ($arResult['SECTIONS'] as &$arSection)
-			{
-				$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-				$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-				?><li id="<? echo $this->GetEditAreaId($arSection['ID']); ?>"><h2 class="bx_catalog_text_title"><a href="<? echo $arSection['SECTION_PAGE_URL']; ?>"><? echo $arSection['NAME']; ?></a><?
-				if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null)
-				{
-					?> <span>(<? echo $arSection['ELEMENT_CNT']; ?>)</span><?
-				}
-				?></h2></li><?
-			}
-			unset($arSection);
-			break;
-		case 'TILE':
-			foreach ($arResult['SECTIONS'] as &$arSection)
-			{
-				$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-				$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-				if (false === $arSection['PICTURE'])
-				{
-					$altValue = (string)($arSection['IPROPERTY_VALUES']['SECTION_PICTURE_FILE_ALT'] ?? '');
-					if ($altValue === '')
-					{
-						$altValue = $arSection['NAME'];
-					}
-					$titleValue = (string)($arSection['IPROPERTY_VALUES']['SECTION_PICTURE_FILE_TITLE'] ?? '');
-					if ($titleValue === '')
-					{
-						$titleValue = $arSection['NAME'];
-					}
-					$arSection['PICTURE'] = array(
-						'SRC' => $arCurView['EMPTY_IMG'],
-						'ALT' => $altValue,
-						'TITLE' => $titleValue,
-					);
-					unset($titleValue, $altValue);
-				}
-				?><li id="<? echo $this->GetEditAreaId($arSection['ID']); ?>">
-				<a
-					href="<? echo $arSection['SECTION_PAGE_URL']; ?>"
-					class="bx_catalog_tile_img"
-					style="background-image:url('<? echo $arSection['PICTURE']['SRC']; ?>');"
-					title="<? echo $arSection['PICTURE']['TITLE']; ?>"
-					> </a><?
-				if ('Y' != $arParams['HIDE_SECTION_NAME'])
-				{
-					?><h2 class="bx_catalog_tile_title"><a href="<? echo $arSection['SECTION_PAGE_URL']; ?>"><? echo $arSection['NAME']; ?></a><?
-					if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null)
-					{
-						?> <span>(<? echo $arSection['ELEMENT_CNT']; ?>)</span><?
-					}
-				?></h2><?
-				}
-				?></li><?
-			}
-			unset($arSection);
-			break;
-		case 'LIST':
-			$intCurrentDepth = 1;
-			$boolFirst = true;
-			foreach ($arResult['SECTIONS'] as &$arSection)
-			{
-				$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-				$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-				if ($intCurrentDepth < $arSection['RELATIVE_DEPTH_LEVEL'])
-				{
-					if (0 < $intCurrentDepth)
-						echo "\n",str_repeat("\t", $arSection['RELATIVE_DEPTH_LEVEL']),'<ul>';
-				}
-				elseif ($intCurrentDepth == $arSection['RELATIVE_DEPTH_LEVEL'])
-				{
-					if (!$boolFirst)
-						echo '</li>';
-				}
-				else
-				{
-					while ($intCurrentDepth > $arSection['RELATIVE_DEPTH_LEVEL'])
-					{
-						echo '</li>',"\n",str_repeat("\t", $intCurrentDepth),'</ul>',"\n",str_repeat("\t", $intCurrentDepth-1);
-						$intCurrentDepth--;
-					}
-					echo str_repeat("\t", $intCurrentDepth-1),'</li>';
-				}
-
-				echo (!$boolFirst ? "\n" : ''),str_repeat("\t", $arSection['RELATIVE_DEPTH_LEVEL']);
-				?><li id="<?=$this->GetEditAreaId($arSection['ID']);?>"><h2 class="bx_sitemap_li_title"><a href="<? echo $arSection["SECTION_PAGE_URL"]; ?>"><? echo $arSection["NAME"];?><?
-				if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null)
-				{
-					?> <span>(<? echo $arSection["ELEMENT_CNT"]; ?>)</span><?
-				}
-				?></a></h2><?
-
-				$intCurrentDepth = $arSection['RELATIVE_DEPTH_LEVEL'];
-				$boolFirst = false;
-			}
-			unset($arSection);
-			while ($intCurrentDepth > 1)
-			{
-				echo '</li>',"\n",str_repeat("\t", $intCurrentDepth),'</ul>',"\n",str_repeat("\t", $intCurrentDepth-1);
-				$intCurrentDepth--;
-			}
-			if ($intCurrentDepth > 0)
-			{
-				echo '</li>',"\n";
-			}
-			break;
-	}
-?>
-</ul>
-<?
-	echo ('LINE' != $arParams['VIEW_MODE'] ? '<div style="clear: both;"></div>' : '');
-}
-?></div>
+    $APPLICATION->IncludeComponent(
+        "bitrix:catalog.section",
+        "category-index",
+        array(
+            "ACTION_VARIABLE" => "action",
+            "ADD_PICT_PROP" => "-",
+            "ADD_PROPERTIES_TO_BASKET" => "Y",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "ADD_TO_BASKET_ACTION" => "ADD",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "BACKGROUND_IMAGE" => "-",
+            "BASKET_URL" => "/personal/basket.php",
+            "BROWSER_TITLE" => "-",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "COMPATIBLE_MODE" => "N",
+            "CONVERT_CURRENCY" => "N",
+            "CUSTOM_FILTER" => "{\"CLASS_ID\":\"CondGroup\",\"DATA\":{\"All\":\"AND\",\"True\":\"True\"},\"CHILDREN\":[]}",
+            "DETAIL_URL" => "",
+            "DISABLE_INIT_JS_IN_COMPONENT" => "N",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "DISPLAY_COMPARE" => "N",
+            "DISPLAY_TOP_PAGER" => "N",
+            "ELEMENT_SORT_FIELD" => "sort",
+            "ELEMENT_SORT_FIELD2" => "id",
+            "ELEMENT_SORT_ORDER" => "asc",
+            "ELEMENT_SORT_ORDER2" => "desc",
+            "ENLARGE_PRODUCT" => "STRICT",
+            "FILTER_NAME" => "arrFilter",
+            "HIDE_NOT_AVAILABLE" => "N",
+            "HIDE_NOT_AVAILABLE_OFFERS" => "N",
+            "IBLOCK_ID" => "4",
+            "IBLOCK_TYPE" => "catalog",
+            "INCLUDE_SUBSECTIONS" => "N",
+            "LABEL_PROP" => array(
+                0 => "HIT",
+            ),
+            "LAZY_LOAD" => "N",
+            "LINE_ELEMENT_COUNT" => "3",
+            "LOAD_ON_SCROLL" => "N",
+            "MESSAGE_404" => "",
+            "MESS_BTN_ADD_TO_BASKET" => "В корзину",
+            "MESS_BTN_BUY" => "Купить",
+            "MESS_BTN_DETAIL" => "Подробнее",
+            "MESS_BTN_LAZY_LOAD" => "Показать ещё",
+            "MESS_BTN_SUBSCRIBE" => "Подписаться",
+            "MESS_NOT_AVAILABLE" => "Нет в наличии",
+            "MESS_NOT_AVAILABLE_SERVICE" => "Недоступно",
+            "META_DESCRIPTION" => "-",
+            "META_KEYWORDS" => "-",
+            "OFFERS_LIMIT" => "5",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => ".default",
+            "PAGER_TITLE" => "Товары",
+            "PAGE_ELEMENT_COUNT" => "18",
+            "PARTIAL_PRODUCT_PROPERTIES" => "N",
+            "PRICE_CODE" => array(
+                0 => "BASE",
+            ),
+            "PRICE_VAT_INCLUDE" => "Y",
+            "PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
+            "PRODUCT_ID_VARIABLE" => "id",
+            "PRODUCT_PROPS_VARIABLE" => "prop",
+            "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+            "PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
+            "PRODUCT_SUBSCRIPTION" => "Y",
+            "PROPERTY_CODE_MOBILE" => array(
+                0 => "ATT_VES",
+            ),
+            "SECTION_CODE" => $_REQUEST["SECTION_CODE"],
+            "SECTION_ID" => $arSection['ID'],
+            "SECTION_ID_VARIABLE" => "SECTION_ID",
+            "SECTION_URL" => "",
+            "SECTION_USER_FIELDS" => array(
+                0 => "UF_VIEW_INDEX",
+                1 => "",
+            ),
+            "SEF_MODE" => "Y",
+            "SET_BROWSER_TITLE" => "Y",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "Y",
+            "SET_META_KEYWORDS" => "Y",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "Y",
+            "SHOW_404" => "N",
+            "SHOW_ALL_WO_SECTION" => "N",
+            "SHOW_CLOSE_POPUP" => "N",
+            "SHOW_DISCOUNT_PERCENT" => "N",
+            "SHOW_MAX_QUANTITY" => "N",
+            "SHOW_OLD_PRICE" => "N",
+            "SHOW_PRICE_COUNT" => "1",
+            "SHOW_SLIDER" => "N",
+            "SLIDER_INTERVAL" => "3000",
+            "SLIDER_PROGRESS" => "N",
+            "TEMPLATE_THEME" => "blue",
+            "USE_ENHANCED_ECOMMERCE" => "N",
+            "USE_MAIN_ELEMENT_SECTION" => "N",
+            "USE_PRICE_COUNT" => "N",
+            "USE_PRODUCT_QUANTITY" => "N",
+            "COMPONENT_TEMPLATE" => "category-index",
+            "SEF_RULE" => "#SECTION_CODE#",
+            "SECTION_CODE_PATH" => "#SECTION_CODE_PATH#",
+            "LABEL_PROP_MOBILE" => array(
+            ),
+            "LABEL_PROP_POSITION" => "top-left"
+        ),
+        false
+    );
+    ?>
+<?endforeach;?>
