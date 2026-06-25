0a6e9f757eedfd98e3164d9ff62ac75cf0295c30
Дмитрий Литвинов <Dlitvinovo@gmail.com>
2026-06-25 18:20:46 +0500

    dev параметры компонента расчета стоимости доставки

--- local/components/ldo/map.delivery/.parameters.php
+++ local/components/ldo/map.delivery/.parameters.php
@@ -1,6 +1,17 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-if(!CModule::IncludeModule("iblock")){
-    return;
-}
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+$arComponentParameters = [
+    'GROUPS' => [
+        'USER_CARD' => [
+            'NAME' => 'Параметры компонента',
+        ],
+    ],
+    'PARAMETERS' => [
+        'OUT_ZONES' => [
+            'NAME' => 'Текст, если адрес вне зон доставок',
+            'TYPE' => 'TEXT',
+            'PARENT' => 'USER_CARD',
+        ]
+    ],
+];
 ?>
