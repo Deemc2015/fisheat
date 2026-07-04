f70cbc562d16f8981c020cea506d2b24837937d2
Дмитрий Литвинов <Dlitvinovo@gmail.com>
2026-07-04 13:21:22 +0500

    dev composite index

--- local/templates/fisheat/components/bitrix/catalog.item/item/result_modifier.php
+++ local/templates/fisheat/components/bitrix/catalog.item/item/result_modifier.php
@@ -4,15 +4,11 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
     die();
 }
 
-// Указываем, что компонент может кешироваться
 $this->__component->SetResultCacheKeys([
     "CACHED_TPL",
     "ID",
     "IBLOCK_ID"
 ]);
-
-// Добавляем в кеш информацию о том, что используется динамическая замена
-$this->__component->arResult["CACHED_TPL"] = ob_get_contents();
 
 
 ?>
