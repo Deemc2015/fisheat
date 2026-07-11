a3bf80a21d4378d0692d395907b8674d0f08ff9e
Дмитрий Литвинов <Dlitvinovo@gmail.com>
2026-07-11 17:13:42 +0500

    fix css

--- local/templates/fisheat/assets/js/main.js
+++ local/templates/fisheat/assets/js/main.js
@@ -146,7 +146,7 @@ $(document).ready(function(){
 
 
     $('#slider-index').on('init', function(event, slick) {
-        console.log('Слайдер запустился');
+
         $(this).removeClass('disabled');
     }).slick({
         slidesToShow: 3,
@@ -262,25 +262,26 @@ $(document).ready(function(){
                     slidesToShow: 4,
                     slidesToScroll: 1,
                     infinite: true,
-                    dots: true
+
                 }
             },
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+
+                }
+            },
+
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
+
                 }
             },
             {
